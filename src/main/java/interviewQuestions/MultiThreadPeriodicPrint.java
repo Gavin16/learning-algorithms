0d564e61f4015d19b9fc40c89c8045bada56fb21
wufangmin <kupature@163.com>
2019-11-19 14:32:59 +0800

    2019-11-19 14:32:53

--- src/main/java/interviewQuestions/MultiThreadPeriodicPrint.java
+++ src/main/java/interviewQuestions/MultiThreadPeriodicPrint.java
@@ -1,5 +1,7 @@
 package interviewQuestions;
 
+import javafx.concurrent.Task;
+
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -17,6 +19,8 @@ public class MultiThreadPeriodicPrint {
 
     /**
      * 方案一: 使用共享变量维护元素打印 位置；并对共享变量的访问加锁控制
+     *
+     * 缺点: 效率较低
      */
 
     private static Lock lock = new ReentrantLock();
@@ -79,20 +83,107 @@ public class MultiThreadPeriodicPrint {
 
 
     /**
-     *   使用 condition 实现线程通知;
+     *  方案二：使用 condition 实现线程通知;
      */
     private static Lock lock2 = new ReentrantLock();
     private static Condition ACondition = lock2.newCondition();
     private static Condition BCondition = lock2.newCondition();
     private static Condition CCondition = lock2.newCondition();
 
+    private static int num = 0;
 
+    static class TaskA implements Runnable{
 
+        @Override
+        public void run() {
+            for(int i = 0 ; i < 10 ; i++){
+                printA(i);
+            }
+        }
+    }
+
+    static class TaskB implements Runnable{
+
+        @Override
+        public void run() {
+            for(int i = 0 ; i < 10 ; i++){
+                printB();
+            }
+        }
+    }
+
+    static class TaskC implements Runnable{
+        @Override
+        public void run() {
+            for(int i = 0 ; i < 10 ; i++){
+                printC();
+            }
+            System.out.println();
+        }
+    }
+
+    static void printA(int i){
+        try {
+            lock2.lock();
+            if(num != 0){
+                ACondition.await();
+            }
+            System.out.print("A");
+            num = (++num)%3;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            BCondition.signal();
+            lock2.unlock();
+        }
+    }
+
+    static void printB(){
+        try {
+            lock2.lock();
+            if(num != 1){
+                BCondition.await();
+            }
+            System.out.print("B");
+            num = (++num)%3;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            CCondition.signal();
+            lock2.unlock();
+        }
+    }
+
+    static void printC(){
+        try {
+            lock2.lock();
+            if(num != 2){
+                CCondition.await();
+            }
+            System.out.print("C");
+            num = (++num)%3;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            ACondition.signal();
+            lock2.unlock();
+        }
+    }
 
 
     public static void main(String[] args) {
-        new ThreadA().start();
-        new ThreadB().start();
-        new ThreadC().start();
+
+        Thread threadA = new Thread(new TaskA());
+        Thread threadB = new Thread(new TaskB());
+        Thread threadC = new Thread(new TaskC());
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+
+
+//        new ThreadB().start();
+//        new ThreadA().start();
+//        new ThreadC().start();
     }
 }
