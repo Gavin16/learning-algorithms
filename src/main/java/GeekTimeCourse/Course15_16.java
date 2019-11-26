9f9ad6ef4af0320bf013d8061ec4403ac2d5cf58
Gavin16 <mingorun@163.com>
2019-11-27 07:58:53 +0800

    完成course16练习

--- src/main/java/GeekTimeCourse/Course15_16.java
+++ src/main/java/GeekTimeCourse/Course15_16.java
@@ -17,12 +17,12 @@ import java.util.Arrays;
 public class Course15_16 {
 
     public static void main(String[]args){
-        int[] arr = ArrayUtil.randValueArray(15);
-        ArrayUtil.showArray(arr);
-        Arrays.sort(arr);
-        ArrayUtil.showArray(arr);
-
-        System.out.println(lastEqualValue(arr,3));
+//        int[] arr = ArrayUtil.randValueArray(15);
+//        ArrayUtil.showArray(arr);
+//        Arrays.sort(arr);
+//        ArrayUtil.showArray(arr);
+        int[] arr = {0,2,2,3,5,6,6,6,7,8,8,10,11,12};
+        System.out.println(lastSmallerValue(arr,6));
     }
 
 
@@ -118,6 +118,22 @@ public class Course15_16 {
      */
     static int firstLargerValue(int[] arr, int n){
 
+        int len = arr.length;
+        int high = len - 1, low = 0;
+
+        while(low <= high){
+            int mid = low + ((high - low) >> 1);
+            if( n > arr[mid]){
+                low = mid + 1;
+            }else{
+                if(mid == 0 || arr[mid - 1] < n){
+                    return mid;
+                }else{
+                    high = mid - 1;
+                }
+            }
+        }
+
         return -1;
     }
 
@@ -125,6 +141,21 @@ public class Course15_16 {
      * 查找最后一个小于等于给定值的元素
      */
     static int lastSmallerValue(int[] arr,int n){
+        int len = arr.length - 1;
+        int high = len, low = 0;
+
+        while(low <= high){
+            int mid = low + ((high - low) >> 1);
+            if(n < arr[mid]){
+                high = mid - 1;
+            }else{
+                if(mid == len || arr[mid + 1] > n){
+                    return mid;
+                }else{
+                    low = mid + 1;
+                }
+            }
+        }
         return -1;
     }
 
