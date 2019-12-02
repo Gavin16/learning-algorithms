3d6541818aa7114acaea8be902f573fd445576c4
wufangmin <kupature@163.com>
2019-12-02 14:21:47 +0800

    循环改递归调用(2)

--- src/main/java/leetCode/RemoveElements.java
+++ src/main/java/leetCode/RemoveElements.java
@@ -24,35 +24,60 @@ import utils.ListNode;
 public class RemoveElements {
 
     public static void main(String[]args){
-        ListNode head1 = LinkedListUtil.genLinkedList(5);
+        ListNode head1 = LinkedListUtil.genLinkedList(9);
 
         LinkedListUtil.showLinkedList(head1);
 
-        ListNode res = removeElements(head1, 3);
+        ListNode res = removeElements1(head1, 2);
         LinkedListUtil.showLinkedList(res);
     }
 
 
     /**
-     * 移除链表中值为 val 的元素
+     * 移除链表中值为 val 的元素 -- 使用哨兵节点
      * @param head
      * @param val
      * @return
      */
     public static ListNode removeElements(ListNode head, int val) {
-        ListNode root = new ListNode(0);
-        ListNode cursor = root;
-        ListNode cur = head;
+        ListNode sentinel = new ListNode(0);
+        ListNode cursor = sentinel;
+        ListNode curr = head;
 
-        while(cur != null){
-            if(cur.val != val){
-                cursor.next = cur;
+        while(curr != null){
+            if(curr.val != val){
+                cursor.next = curr;
                 cursor = cursor.next;
             }
-            cur = cur.next;
+            curr = curr.next;
         }
         cursor.next = null;
 
-        return root.next;
+        return sentinel.next;
     }
+
+
+    /**
+     * 递归移除链表中值为 val 的元素
+     * 循环改递归(2)  -- (√)
+     */
+    public static ListNode removeElements1(ListNode head, int val) {
+        return recursiveRemove(head,val);
+    }
+
+    static ListNode recursiveRemove(ListNode head,int val){
+        if(head == null) return head;
+
+        if(head.val == val){
+            while(head != null && head.val == val){
+                head = head.next;
+            }
+            head = recursiveRemove(head,val);
+        }else{
+            head.next = recursiveRemove(head.next,val);
+        }
+
+        return head;
+    }
+
 }
