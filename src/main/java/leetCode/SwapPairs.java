3f87ab9b795bb79ad3b054d2dc80545f94337d3a
wufangmin <kupature@163.com>
2019-12-02 17:45:53 +0800

    2019-12-2 17:45:49

--- src/main/java/leetCode/SwapPairs.java
+++ src/main/java/leetCode/SwapPairs.java
@@ -1,5 +1,6 @@
 package leetCode;
 
+import utils.LinkedListUtil;
 import utils.ListNode;
 
 /**
@@ -25,16 +26,23 @@ import utils.ListNode;
  */
 public class SwapPairs {
 
+    public static void main(String[] args) {
+        ListNode listNode = LinkedListUtil.genLinkedList(7);
+
+        LinkedListUtil.showLinkedList(listNode);
+
+        LinkedListUtil.showLinkedList(swapPairs1(listNode));
+    }
+
     /**
      * @param head
      * @return
      */
-    public ListNode swapPairs(ListNode head) {
+    static ListNode swapPairs(ListNode head) {
         if(head == null || head.next == null) return head;
         ListNode pre = head , suf = head.next , res = suf , lp = null;
         while(pre != null && suf != null){
             ListNode next1 = suf.next;
-
             suf.next = pre ;
             pre.next = next1;
             // 保证每个两两交换之间的指向关系不断
@@ -47,7 +55,31 @@ public class SwapPairs {
             }else{
                 break;
             }
+        }
+        return res;
+    }
 
+    /**
+     *  写出来和第一次实现差不多，代码结构相对变简单一些了
+     */
+    static ListNode swapPairs1(ListNode head){
+        if(head == null || head.next == null) return head;
+
+        ListNode odd = head, even = head.next, res = even , prev = null;
+        while(odd != null && even != null){
+            ListNode temp = even.next;
+            even.next = odd;
+            odd.next = temp;
+            if(prev != null){
+                prev.next = even;
+            }
+
+            prev = odd;
+            odd = odd.next;
+
+            if(odd != null) {
+                even = odd.next;
+            }
         }
         return res;
     }
