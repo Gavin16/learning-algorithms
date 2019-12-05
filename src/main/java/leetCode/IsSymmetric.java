bed7a2d602190bf5bf3c2de63a369bcf610fd897
wufangmin <kupature@163.com>
2019-12-05 19:00:58 +0800

    增加leetCode101 判断二叉树是否对称

---
+++ src/main/java/leetCode/IsSymmetric.java
@@ -0,0 +1,28 @@
+package leetCode;
+
+import dataStruct.TreeNode;
+
+/**
+ * 《LeetCode 101. 对称二叉树》
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *
+ * 运用递归和迭代两种方法解决这个问题.
+ */
+public class IsSymmetric {
+
+    public static void main(String[] args) {
+
+    }
+
+    static boolean isSymmetric(TreeNode root) {
+        return false;
+    }
+}
