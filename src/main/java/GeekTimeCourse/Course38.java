ce0b6320901d2139e47a9c7a6a315f27a870386c
Gavin16 <mingorun@163.com>
2020-01-11 15:03:26 +0800

    完成外部排序

--- src/main/java/GeekTimeCourse/Course38.java
+++ src/main/java/GeekTimeCourse/Course38.java
@@ -1,12 +1,16 @@
 package GeekTimeCourse;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * @Description: 分治算法练习题
  * (1) 对外部大文件排序
- * (2) 二维平面上有 n 个点，如何快速计算出两个距离最近的点对
- * (3) 有两个 n*n 的矩阵 A，B，如何快速求解两个矩阵的乘积 C=A*B？
+ * (2) 二维平面上有 n 个点，如何快速计算出两个距离最近的点对 （TODO）
+ * (3) 有两个 n*n 的矩阵 A，B，如何快速求解两个矩阵的乘积 C=A*B？（TODO）
  */
 public class Course38 {
 
@@ -24,30 +28,31 @@ public class Course38 {
      *  最后排好序的文件统一存放在: D:\DataCenter\algoData\sortScores\sorted.csv
      */
     public static void main(String[] args) throws IOException {
-        bigDataOuterSort();
+//        splitToDataSegments();
+        sortAndMerge();
     }
 
     private static String segmentsPath = "D:\\DataCenter\\algoData\\sortScores\\segments\\";
+    private static String outputPath = "D:\\DataCenter\\algoData\\sortScores\\output\\";
+
+    private static String origPath = "D:\\dataCenter\\algoData\\sortScores\\exam_scores.csv";
 
     private static final int fileNums = 250;
 
 
-    static void bigDataOuterSort() throws IOException {
+    /** 遍历拆分文件 */
+    static void splitToDataSegments() throws IOException {
         // 设置虚拟机内存 : 让可用内存小于数据总大小？？
         //  -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
-        String origPath = "D:\\dataCenter\\algoData\\sortScores\\exam_scores.csv";
-
         File file = new File(origPath);
         FileReader fr = new FileReader(file);
         BufferedReader br = new BufferedReader(fr);
-
 
         // 得分分段的段长度
         int step = 750 / fileNums;
         // 创建包含 fileNums 个file文件的数组
         File[] segFiles = preGenSegmentFiles(fileNums);
         // 创建 fileNums 个 BufferedWriter
-//        BufferedWriter[] writers = preGenBuferedWriters(segFiles);
 
         // 遍历一遍原大文件，将数据分段保存到 segments 路径，分段方式 : [0-3),[3,6),[6,9) ... [747,750)
         String line;
@@ -70,29 +75,51 @@ public class Course38 {
                 bw.close();
             }
         }
-        // 关闭输出流
-
-
-        // 读取各个文件排序后写回该文件
-
-        // 将各个文件合并成大的有序文件
     }
 
-    private static void closeBufferedWriters(BufferedWriter[] writers) throws IOException {
-        for(int k = 0; k < writers.length ; k++){
-            writers[k].close();
+
+    private static void sortAndMerge() throws IOException {
+        File[] segFiles = preGenSegmentFiles(fileNums);
+        String path = outputPath  + "sorted_scores.csv";
+
+        BufferedWriter resBr = new BufferedWriter(new FileWriter(path));
+
+        List<String[]> allScores = new ArrayList<>();
+        // 分数段从高到低读取所有文件
+        for(int j = segFiles.length-1 ; j >= 0 ; j --){
+            BufferedReader br = new BufferedReader(new FileReader(segFiles[j]));
+
+            // 读取所有行数据，做快速排序
+            allScores.clear();
+            String line;
+            while(null != (line = br.readLine())){
+                String[] scores = line.split(",");
+                allScores.add(scores);
+            }
+
+            // 倒序排序分数段内的成绩
+            Collections.sort(allScores, new Comparator<String[]>() {
+                @Override
+                public int compare(String[] o1, String[] o2) {
+                    String s1 = o1[o1.length-1];
+                    String s2 = o2[o2.length-1];
+
+                    int n1 = Integer.parseInt(s1);
+                    int n2 = Integer.parseInt(s2);
+                    return n2 - n1;
+                }
+            });
+
+            // 排序结果写入文件
+            for(String[] arr : allScores){
+                String record = String.join(",",arr) + "\r\n";
+                resBr.write(record,0,record.length());
+                resBr.flush();
+            }
+
         }
     }
 
-
-    private static BufferedWriter[] preGenBuferedWriters(File[] segFiles) throws IOException {
-        BufferedWriter[] writers = new BufferedWriter[segFiles.length];
-        for(int j = 0 ; j < segFiles.length ; j++){
-            BufferedWriter bw = new BufferedWriter(new FileWriter(segFiles[j]));
-            writers[j] = bw;
-        }
-        return writers;
-    }
 
     private static File[] preGenSegmentFiles(int num) throws IOException {
         File[] segFiles = new File[num];
