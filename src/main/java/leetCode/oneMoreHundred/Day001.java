d5e5524b43ed993c3d3ad2028f8360e1a2fb7587
wufangmin <kupature@163.com>
2020-07-01 12:58:11 +0800

    刷题day001

--- src/main/java/leetCode/oneMoreHundred/Day001.java
+++ src/main/java/leetCode/oneMoreHundred/Day001.java
@@ -2,17 +2,21 @@ package leetCode.oneMoreHundred;
 
 import utils.ArrayUtil;
 
+import java.util.Stack;
+
 
 /**
  * 283 移动零/155. 最小栈
  * @Date： 2020年6月30日
  * ==============================================================================
  * 《283 移动零》
+ * ==============================================================================
  * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
  * 必须在原数组上操作，不能拷贝额外的数组。
  * 尽量减少操作次数
  * ==============================================================================
  * 《155. 最小栈》
+ * ==============================================================================
  * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
  * push(x) —— 将元素 x 推入栈中。
  * pop() —— 删除栈顶的元素。
@@ -28,6 +32,26 @@ public class Day001 {
 //        moveZeroes1(input);
         moveZeroes2(input);
         ArrayUtil.showArray(input);
+
+        // test 最小栈
+        testMinStack();
+
+    }
+
+    private static void testMinStack() {
+        MinStack minStack = new MinStack();
+
+        minStack.push(512);
+        minStack.push(-1024);
+        minStack.push(-1024);
+        minStack.push(512);
+
+        minStack.pop();
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.getMin());
     }
 
 
@@ -68,28 +92,39 @@ public class Day001 {
 
     /**
      * @Title: 155. 最小栈
+     * @Version: 版本1 使用辅助栈存储数据
      */
-    class MinStack {
+    static class MinStack {
+
+        private Stack<Integer> data;
+        private Stack<Integer> helper;
 
         /** initialize your data structure here. */
         public MinStack() {
-
+            this.data = new Stack();
+            this.helper = new Stack();
         }
 
         public void push(int x) {
-
+            data.push(x);
+            if(helper.empty() || helper.peek() >= x){
+                helper.push(x);
+            }
         }
 
         public void pop() {
-
+            Integer pop = data.pop();
+            if(helper.peek() == pop.intValue()){
+                helper.pop();
+            }
         }
 
         public int top() {
-            return -1;
+            return data.peek();
         }
 
         public int getMin() {
-            return -1;
+            return helper.peek();
         }
     }
 
