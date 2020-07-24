3447bb2580b0f6c89b341212dd3f870cf8274687
Gavin16 <kupature@163.com>
2020-07-25 01:34:30 +0800

    Day011 合并区间版本1

--- src/main/java/leetCode/oneMoreHundred/Day011.java
+++ src/main/java/leetCode/oneMoreHundred/Day011.java
@@ -1,11 +1,15 @@
 package leetCode.oneMoreHundred;
 
+import utils.ArrayUtil;
+
+import java.util.*;
+
 /**
  * @Date：2020年7月23日
  * ==============================================================================
  * 56. 合并区间
  * ==============================================================================
- * 给出一个区间的集合，请合并所有重叠的区间。
+ * 给出一个区间的集合，请合并所有重叠的区间
  *
  * 示例 1:
  *
@@ -21,13 +25,49 @@ package leetCode.oneMoreHundred;
  */
 public class Day011 {
 
+
+    public static void main(String[] args) {
+        int[][] dim2Arr = {{1,3},{2,6},{8,10},{15,18}};
+        int[][] dim2Arr1 = {{1,4},{4,5}};
+        int[][] dim2Arr2 = {{2,3},{4,5},{6,7},{8,9},{1,10}};
+        int[][] merge = merge(dim2Arr2);
+        for(int[] arr : merge){
+            ArrayUtil.showArray(arr);
+        }
+    }
+
+
+
     /**
      * @title: 56. 合并区间
+     * @version: 版本1
      * @version:
      * @param intervals
      * @return
      */
-    public int[][] merge(int[][] intervals) {
+    public static int[][] merge(int[][] intervals) {
+        if(intervals.length < 2) return intervals;
 
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+        List<int[]> arrList = new ArrayList<>();
+        int[] temp = new int[2];
+        temp[0] = intervals[0][0];
+        temp[1] = intervals[0][1];
+        for(int i = 1 ; i < intervals.length ; i++){
+            if(intervals[i][0] <= temp[1]){
+                temp[1] = Math.max(temp[1],intervals[i][1]);
+            }else{
+                int[] res = Arrays.copyOf(temp,2);
+                arrList.add(res);
+                temp[0] = intervals[i][0];
+                temp[1] = intervals[i][1];
+            }
+        }
+        int[] res = Arrays.copyOf(temp,2);
+        arrList.add(res);
+
+        int[][] ret = new int[arrList.size()][2];
+        arrList.toArray(ret);
+        return ret;
     }
 }
