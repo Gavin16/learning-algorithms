dbed3312872efc961b6386e67ea4c00fcef0251f
wufangmin <mingorun@163.com>
2021-01-30 23:07:31 +0800

    严格线性跌代 斐波那契数列解法，增加矩阵计算工具类

---
+++ src/main/java/datastruct/Matrix.java
@@ -0,0 +1,89 @@
+package datastruct;
+
+import java.util.Objects;
+
+/**
+ * 整数矩阵工具类
+ */
+public class Matrix {
+
+
+    public static void main(String[] args) {
+        int[][] test = {{1,1},{1,0}};
+//        int[][] e = {{1,0},{0,1}};
+
+        int[][] multiply = multiply(test, test);
+        int[][] power = power(test, 3);
+
+        showMatrix(multiply);
+        showMatrix(power);
+    }
+
+    /**
+     * 两个矩阵相乘，返回相乘后的结果
+     *
+     * 矩阵相乘需要满足 左乘矩阵的列数 = 右乘矩阵的行数
+     * 如 m*n   乘以  n*l  =》 m*l 的矩阵返回
+     *
+     * @param m1
+     * @param m2
+     * @return
+     */
+    public static int[][] multiply(int[][]m1 ,int[][]m2){
+        if(null == m1 || null == m2 || m1.length == 0 || m2.length ==0) return null;
+        if(m1[0].length != m2.length) return null;
+
+        int rows = m1.length , columns = m2[0].length;
+        int[][] res = new int[rows][columns];
+
+        for(int i = 0; i < res.length ; i++){
+            for(int j = 0 ; j < res[0].length ; j++){
+                res[i][j] = 0 ;
+                for(int k = 0 ; k < m1[0].length ; k++){
+                    res[i][j] += m1[i][k] * m2[k][j];
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 计算矩阵的 e 次方(矩阵必须是方阵)
+     * @param m
+     * @param e
+     * @return
+     */
+    public static int[][] power(int[][]m ,int e){
+        if(Objects.isNull(m) || m.length != m[0].length) return null;
+        int[][] res = new int[m.length][m[0].length];
+        for(int k = 0 ; k < res.length ;k++){
+            res[k][k] = 1;
+        }
+        int tmp = e;
+        while(tmp != 0){
+            int bit = tmp & 1;
+            if(bit == 1){
+                res = multiply(m,res);
+            }
+            m = multiply(m,m);
+            tmp = tmp >>> 1;
+        }
+        return res;
+    }
+
+    /**
+     * 打印输出矩阵内容
+     * @param matrix
+     */
+    public static void showMatrix(int[][] matrix){
+        if(Objects.isNull(matrix) || 0 == matrix.length) return;
+        int R = matrix.length , C = matrix[0].length;
+
+        for(int i = 0 ; i < R ; i++){
+            for(int j = 0 ; j < C ;j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- src/main/java/leetcode/oneMoreHundred/Day029.java
+++ src/main/java/leetcode/oneMoreHundred/Day029.java
@@ -1,5 +1,7 @@
 package leetcode.oneMoreHundred;
 
+import datastruct.Matrix;
+
 /**
  * 4. 寻找两个正序数组的中位数
  * ==============================================================================
@@ -14,6 +16,17 @@ package leetcode.oneMoreHundred;
  * 解释：合并数组 = [1,2,3] ，中位数 2
  * ==============================================================================
  *
+ * 第2节 斐波那契数列递归
+ *
+ * 斐波那契数列作为严格递归数列满足如下关系:
+ *
+ * [Fn ,Fn-1] = [Fn-1,Fn-2]* C
+ * 其中矩阵C 为
+ * 「1，1
+ *   1，0」
+ *
+ *
+ *
  */
 public class Day029 {
 
@@ -26,5 +39,53 @@ public class Day029 {
      */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         return 0;
+    }
+
+
+    /**
+     * 快速求一个数的指数次方
+     *
+     * 将指数视为二进制，并对基数循环的做平方处理，
+     * 每次处理根据二进制比特位判断是否需要乘进结果里
+     *
+     * @param m
+     * @param e
+     * @return
+     */
+    public long fastPower(int m , int e){
+        long res = 1;
+        long tmp = e , mm = m;
+        while(tmp != 0){
+            long bit = tmp & 1;
+            if(bit == 1){
+                res *= mm;
+            }
+            mm *= mm;
+            tmp = tmp >>> 1;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Day029 day029 = new Day029();
+        System.out.println(day029.fastPower(2,16));
+
+        System.out.println("================");
+        System.out.println(day029.Fibonacci1(4));
+    }
+
+    /**
+     * f(1) = 1,f(2）= 2, f(3) = 3 ... f(n) = f(n-1) + f(n-2)
+     *
+     * @param n
+     * @return
+     */
+    public int Fibonacci1(int n){
+        if(n < 1) return 0;
+        if(n == 1 || n == 2) return n;
+        int[][] convertor = {{1,1},{1,0}};
+
+        int[][]res = Matrix.power(convertor,n-2);
+        return 2*res[0][0] + res[1][0];
     }
 }
