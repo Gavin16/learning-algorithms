d9daaa1906390c1b04b796f4351a96002d8c9906
minsky <mingorun@163.com>
2021-05-16 17:54:49 +0800

    leetcode-05月16日

--- src/main/java/leetcode/oneMoreHundred/Day033.java
+++ src/main/java/leetcode/oneMoreHundred/Day033.java
@@ -3,6 +3,8 @@ package leetcode.oneMoreHundred;
 import datastruct.ListNode;
 import utils.LinkedListUtil;
 
+import java.util.Arrays;
+
 /**
  * 92. 反转链表 II
  *
@@ -11,6 +13,19 @@ import utils.LinkedListUtil;
  *
  * 输入：head = [1,2,3,4,5], left = 2, right = 4
  * 输出：[1,4,3,2,5]
+ *
+ *
+ * 221. 最大正方形
+ * 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+ *
+ * 输入：matrix = [
+ *                  ["1","0","1","0","0"],
+ *                  ["1","0","1","1","1"],
+ *                  ["1","1","1","1","1"],
+ *                  ["1","0","0","1","0"]
+ *               ]
+ * 输出：4
+ *
  */
 public class Day033 {
 
@@ -20,6 +35,12 @@ public class Day033 {
 
         ListNode reverse = reverseBetween1(listNode, 2, 5);
         LinkedListUtil.showLinkedList(reverse);
+
+
+        System.out.println("------------最大正方形-----------");
+        int[][] input = {{1,0,1,0,0},{1,0,1,1,1},{1,1,1,1,1},{1,0,0,1,0}};
+
+
     }
 
 
@@ -104,5 +125,45 @@ public class Day033 {
             guard.next = forFirst;
         }
         return sentinel.next;
+    }
+
+
+    /**
+     * 动态规划解法
+     * 定义dp[i,j] 为以(i,j)坐标作为右下角能构成全"1" 正方形的做大边长;
+     * 则dp[i,j] = 0                                              若 matrix[i][j] = 0
+     *           = 1 + Math.min(dp[i-1,j],dp[i,j-1],dp[i-1,j-1])  若 matrix[i][j] = 1
+     *
+     * dp[i,j] 第0行和第0列可以直接以matrix 的第0行和第0列进行初始化；
+     * dp二维表迭代更新值时，可以从左到右更新，也可以从上到下更新
+     *
+     * @param matrix
+     * @return
+     */
+    public static int maximalSquare(char[][] matrix) {
+        int R = matrix.length, C = matrix[0].length;
+        int[][] dp = new int[R][C];
+        // 初始化DP表
+        for(int i = 0 ; i < R ;i++){
+            dp[i][0] = matrix[i][0] - '0';
+        }
+        for(int j = 0 ; j < C ;j++){
+            dp[0][j] = matrix[0][j] - '0';
+        }
+        for(int r = 1 ; r < R ; r++){
+            for(int c = 1 ; c < C ; c++){
+                if(matrix[r][c] == '0') dp[r][c] = 0;
+                else{
+                    dp[r][c] = 1 + Math.min(dp[r-1][c],Math.min(dp[r][c-1],dp[r-1][c-1]));
+                }
+            }
+        }
+        int max = 0;
+        for(int[] row : dp){
+            for(int ele : row){
+                if(ele > max) max = ele;
+            }
+        }
+        return max*max;
     }
 }
