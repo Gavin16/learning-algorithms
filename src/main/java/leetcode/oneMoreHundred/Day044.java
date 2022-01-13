2a1569f2a44ac2daaea38238e61fe68344ba048e
minsky <mingorun@163.com>
2022-01-13 15:21:49 +0800

    深度优先搜索十连

---
+++ src/main/java/leetcode/oneMoreHundred/Day044.java
@@ -0,0 +1,93 @@
+package leetcode.oneMoreHundred;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * 深度优先搜索: 1/10
+ * 《200. 岛屿数量》
+ *
+ * 给你一个由'1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ * 此外，你可以假设该网格的四条边均被水包围。
+ *
+ * 示例 1：
+ * 输入：grid = [
+ *   ["1","1","1","1","0"],
+ *   ["1","1","0","1","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","0","0","0"]
+ * ]
+ * 输出：1
+ * 示例 2：
+ *
+ * 输入：grid = [
+ *   ["1","1","0","0","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","1","0","0"],
+ *   ["0","0","0","1","1"]
+ * ]
+ * 输出：3
+ *
+ * 提示：
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 300
+ * grid[i][j] 的值为 '0' 或 '1'
+ *
+ */
+public class Day044 {
+
+    /**
+     * 深度优先搜索
+     */
+    public int numIslands(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int isLands = 0;
+        for(int i = 0 ; i < rows ; i++){
+            for(int j = 0 ; j < cols; j++){
+                isLands += dfs(grid, i, rows, j, cols);
+            }
+        }
+        return isLands;
+    }
+
+    /**
+     * 深度优先搜索 & 翻转字符
+     *
+     * 需判断方框边界
+     */
+    private int dfs(char[][]grid, int i ,int rows, int j, int cols){
+        if((i >= 0 && i < rows) && (j >= 0 && j < cols)){
+            if(grid[i][j] == '1'){
+                grid[i][j]  = '0';
+                dfs(grid, i-1, rows, j, cols);
+                dfs(grid, i+1, rows, j, cols);
+                dfs(grid, i, rows, j-1, cols);
+                dfs(grid, i, rows, j+1, cols);
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        char[][] grid = {{'1','1','0','0','0'},
+                        {'1','1','0','0','0'},
+                        {'0','0','1','0','0'},
+                        {'0','0','0','1','1'}};
+
+        Day044 instance = new Day044();
+        int num = instance.numIslands(grid);
+
+        System.out.println(num);
+        for(char[] chars : grid){
+            System.out.println(JSON.toJSON(chars));
+        }
+
+
+    }
+
+}
