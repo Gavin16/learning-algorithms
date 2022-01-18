86b01288bd58068c1df6cdd386e73f86839be357
minsky <mingorun@163.com>
2022-01-18 09:57:58 +0800

    跳跃游戏I

--- src/main/java/leetcode/oneMoreHundred/Day045.java
+++ src/main/java/leetcode/oneMoreHundred/Day045.java
@@ -26,6 +26,31 @@ package leetcode.oneMoreHundred;
  * 1 <= nums.length <= 104
  * 0 <= nums[i] <= 1000
  *
+ ********************** ********************** ********************** **********************
+ *
+ *《55. 跳跃游戏》
+ * 给定一个非负整数数组nums ，你最初位于数组的 第一个下标 。
+ *
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * 判断你是否能够到达最后一个下标。
+ *
+ * 示例1：
+ * 输入：nums = [2,3,1,1,4]
+ * 输出：true
+ * 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+ *
+ * 示例2：
+ * 输入：nums = [3,2,1,0,4]
+ * 输出：false
+ * 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+ * 
+ *
+ * 提示：
+ * 1 <= nums.length <= 3 * 104
+ * 0 <= nums[i] <= 105
+ *
+ *  ********************** ********************** ********************** **********************
  */
 public class Day045 {
 
@@ -101,11 +126,40 @@ public class Day045 {
     }
 
 
+
+
+    /**
+     *  思路:
+     *  从左向右遍历，动态保存所有位置能跳转的最远位置
+     *  每到一个新位置,判断当前值是否为0 且最远跳转位置<= 当前位置
+     *  若是则不能跳转到最后
+     *  否则可以跳转到最后位置
+     *
+     */
+    public boolean canJump(int[] nums) {
+        if(null == nums || nums.length <= 1) return true;
+        int len = nums.length;
+        int maxPos = 0;
+        for(int i = 0; i < len; i++){
+            maxPos = Math.max(maxPos, i + nums[i]);
+            if(maxPos >= len - 1) return true;
+            if(nums[i] == 0 && maxPos <= i){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+
     public static void main(String[] args) {
 //        int[]nums = {5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5};
 //        int[] nums = {2,3,0,1,4};
         int[] nums =  {2,3,1,2,4,2,3};
+        int[] nums1 = {3,2,1,0,4};
         Day045 instance = new Day045();
-        System.out.println(instance.jump1(nums));
+//        System.out.println(instance.jump1(nums));
+        System.out.println(instance.canJump(nums1));
     }
 }
